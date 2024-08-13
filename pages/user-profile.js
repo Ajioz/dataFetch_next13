62414fde5bc6f76e134ee7d545cacf08f93f2626
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-13 14:42:35 +0100

    added dynamic data fetching

---
+++ pages/user-profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const UserProfilePage = (props) => {
+  return (
+    <div>
+      <h1>User: {props.username}</h1>
+    </div>
+  );
+};
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  const {params, req, res} = context;
+  return {
+    props: { username: "Ajioz" },
+  };
+}
